b7147c5d90630d57e12c54ee3e82c17d9d04c531
vokborok <korobkovsa@gmail.com>
2021-05-09 00:18:44 +0300

    Add files via upload

---
+++ OOP/third_class.py
@@ -0,0 +1,10 @@
+from second_class import SecondClass
+class ThirdClass(SecondClass):
+    def __init__(self, value):
+        self.data = value
+    def __add__(self, other):
+        return ThirdClass(self.data + other)
+    def __str__(self):
+        return '[ThirdClass: %s]' % self.data
+    def mul(self, other):
+        self.data *= other
